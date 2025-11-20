76bf26a126278d417a473ff64524b0a485d1e2b3
FlatBushZombies <brianmakanjira18@gmail.com>
2025-11-20 17:28:03 +0200

    add sitemap xml

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://oakwoodesl.com', 
+  generateRobotsTxt: true,         
+  sitemapSize: 5000,                  
+  changefreq: 'weekly',               
+  priority: 0.7,                      
+  exclude: ['/secret-page'],          
+ 
+}
